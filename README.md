8554fe768c0c44f6d0c9c0e17bcd13116d5bd3bd
nmorse <morse.nate@gmail.com>
2020-10-18 19:02:24 -0400

    proxy with a netlify function

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
  * Jira API
  * Ramda
 
+### Why no CORS
 jira.atlassian.com/rest/api/latest/search?jql=labels=affects-server
- 
+curl -i -X OPTIONS jira.atlassian.com/rest/api/latest/search?jql=labels=affects-server -H 'Access-Control-Request-Method: GET' -H 'Access-Control-Request-Headers: Content-Type, Accept' -H 'Origin: http://localhost:3000' 
---
+++ functions/get-jira-demo/get-jira-demo.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context) {
+  try {
+    const jiraUrl = `https//jira.atlassian.com/rest/api/latest/search?jql=labels=affects-server`;
+    const response = await fetch(jiraUrl, {
+      headers: { Accept: "application/json" }
+    });
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data })
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+};
